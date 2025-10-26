962447ed24b7dab626ec007cfd4ddcca35c907e2
mohammed454545 <mohammed.h.salh@gmail.com>
2025-10-26 19:04:37 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import * as React from 'react';
 import TopAppBar from './components/TopAppBar';
+import FooterBar from './components/FooterBar';
+import Box from '@mui/material/Box';
 
-function App() {
+export default function App() {
   return (
-    <>
+    <Box sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
       <TopAppBar />
-      {/* محتوى صفحاتك هنا */}
-    </>
+      {/* محتوى الصفحات */}
+      <Box sx={{ flex: 1 }}>
+        {/* ... */}
+      </Box>
+      <FooterBar />
+    </Box>
   );
 }
-
-export default App;
